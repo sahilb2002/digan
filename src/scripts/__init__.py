8970e3c85b1f919d0537c3d9d6e2f3f6559ca988
sahil bansal <sahilb20@iitk.ac.in>
2022-03-17 03:54:03 +0530

    done small changes

--- src/scripts/__init__.py
+++ src/scripts/__init__.py
@@ -6,7 +6,7 @@ import os
 
 
 def save_video_grid(img,outdir, drange, normalize=True):
-    C,T,H,W = img[0].shape
+    _, C ,T ,H ,W = img[0].shape
     print (f'Saving Video with {T} frames, img shape {H}, {W}')
 
     for i in range(len(img)):
@@ -19,10 +19,11 @@ def save_video_grid(img,outdir, drange, normalize=True):
         # gw, gh = grid_size
         # _N, C, T, H, W = img.shape
         # img = img.reshape(gh, gw, C, T, H, W)
-        # img = img.transpose(3, 0, 4, 1, 5, 2)
-        img[i] = img[i].reshape(T, H,  W, C)
+        img[i] = np.squeeze(img[i])
+        img[i] = img[i].transpose(1,2,3,0)
+        # img[i] = img[i].reshape(T, H,  W, C)
         # assert C in [3]
         if C == 3:
             torchvision.io.write_video(os.path.join(outdir,f'video{i}.mp4'), torch.from_numpy(img[i]), fps=8)
-            imgs = [PIL.Image.fromarray(img[i], 'RGB') for i in range(len(img))]
+            # imgs = [PIL.Image.fromarray(img[i], 'RGB') for i in range(len(img))]
             # imgs[0].save(fname, quality=95, save_all=True, append_images=imgs[1:], duration=100, loop=0)
